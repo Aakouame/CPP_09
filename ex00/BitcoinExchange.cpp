a6da346192668308d3df1a89f50fda2051289f58
Aissa Kouame <akouame@e2r3p11.1337.ma>
2023-05-29 16:22:53 +0100

    UP

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: akouame <akouame@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 08:08:07 by akouame           #+#    #+#             */
-/*   Updated: 2023/05/29 09:37:52 by akouame          ###   ########.fr       */
+/*   Updated: 2023/05/29 16:21:38 by akouame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,14 +43,17 @@ int	checkDate(std::string	date)
 		if (date[i] < '0' || date[i] > '9')
 			return (1);
 	}
+	int tmp = atoi(date.substr(0, 4).c_str());
+	if (tmp < 2009)
+		return (4);
 	if (date[4] != '-')
 		return (2);
 	for (int i = 5; i < 7; i++){
 		if (date[i] < '0' || date[i] > '9')
 			return (3);
 	}
-	int tmp = atoi(date.substr(5, 6).c_str());
-	if (tmp > 12)
+	tmp = atoi(date.substr(5, 7).c_str());
+	if (tmp > 12 || tmp <= 0)
 		return (4);
 	if (date[7] != '-')
 		return (4);
@@ -59,7 +62,7 @@ int	checkDate(std::string	date)
 			return (5);
 	}
 	tmp = atoi(date.substr(8, 9).c_str());
-	if (tmp > 30)
+	if (tmp > 30 || tmp <= 0)
 		return (6);
 	return (0);
 }
@@ -67,13 +70,22 @@ int	checkDate(std::string	date)
 int	checkValue(std::string	value)
 {
 	int	counter = 0;
-	int	nb;
+	long	nb;
+	int	s = 0;
+	size_t	j = 0;
+	if(value == "")
+		return (10);
 	if (value[0] == '.')
 		return (1);
-	for (size_t i = 0; i < value.size(); i++){
+	if (value[0] == '-')
+	{
+		s++;
+		j++;	
+	}
+	for (size_t i = j; i < value.size(); i++){
 		if (value[i] < '0' || value[i] > '9' )
 		{
-			if (value[i] == '.')
+			if (value[i] == '.' && value[i+1])
 				counter++;
 			else
 				return (2);
@@ -82,7 +94,7 @@ int	checkValue(std::string	value)
 	if (counter > 1)
 		return (3);
 	nb = atof(value.c_str());
-	if (nb < 0)
+	if (s == 1)
 		return (4);
 	if (nb > 1000)
 		return (5);
@@ -119,19 +131,16 @@ void    Bitcoinexchange::parse_map(std::string fileName, int i){
 			if (i == 1)
 			{
 				date = line.substr(0, line.find(' '));
-				
-				// std::cout << "date = " << date << std::endl;
+				if (checkDate(date)){
+					std::cout << "Error: bad input => " << date << std::endl;
+					continue ;
+				}
 				if (line.substr(10, 3) != " | ")
 				{
 					std::cout << "Error: bad input => should be separated by \" | \"" << std::endl;
 					continue;
 				}
-				if (checkDate(date)){
-					std::cout << "Error: bad input => " << date << std::endl;
-					continue ;
-				}
-				value = line.substr(13, line.size() - 1);
-				// std::cout << "Value = " << value << std::endl;
+				value = line.substr(13, 5);
 				if (checkValue(value))
 				{
 					if (checkValue(value) == 4)
@@ -144,14 +153,18 @@ void    Bitcoinexchange::parse_map(std::string fileName, int i){
 				if (!checkDate(date) && !checkValue(value))
 				{
 					std::map<std::string, float>::iterator	it = _database.find(date);
-					if (it != _database.end())
-						std::cout << date << " => " << value << " = " << (atof(value.c_str()) * it->second);
+					if (it == _database.end())
+					{
+						it = _database.lower_bound(date);
+						it--;
+					}
+					std::cout << date << " => " << value << " = " << (atof(value.c_str()) * it->second);
 					std::cout << std::endl;
 				}
 			}
 			else
 			{
-				date = line.substr(0, line.find(',') - 1);
+				date = line.substr(0, line.find(','));
 				value = line.substr(line.find(',') + 1);
 				_database.insert(std::make_pair(date,atof(value.c_str())));
 			}
@@ -168,8 +181,4 @@ void    Bitcoinexchange::parse(const char *av)
     
 	parse_map("data.csv", 0);
 	parse_map(_inputFile, 1);
-	std::map<std::string, float>::iterator it;
-    // for (it = _database.begin(); it != _database.end(); ++it) {
-    //     std::cout << it->first << ": " << it->second << std::endl;
-    // }
 }
